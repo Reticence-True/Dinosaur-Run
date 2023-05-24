1cd918f70c926087afe659ea649ba5f85d07636e
Reticent <1219380015@qq.com>
2023-05-24 14:54:43 +0800

    更新碰撞算法

--- 恐龙快跑/source/motion.js
+++ 恐龙快跑/source/motion.js
@@ -1,16 +1,38 @@
+// #region 全局变量
+const  // 设置速度(默认为3)
+    g = 9.8 // 重力加速度
+    ;
+var speed = 3
+// #endregion
 
-let speed = 3 // 设置速度(默认为3)
+// #region 背景物体生成时间
+// 物体生成时间
+var summomInterval = 0, preSummonInterval = 0
+
+function spawnObject() {
+    summomInterval = Math.floor(Math.random() * 5000)
+    if (Math.abs(preSummonInterval - summomInterval) <= 3500) {
+        preSummonInterval = summomInterval
+    } else {
+        // console.log("生成");
+        createObstacle()
+        createCloud()
+    }
+}
+var summonTimer = setInterval(spawnObject, 10)
+
+// #endregion
 
 // #region 背景地面的移动
-let ground = document.getElementsByClassName("ground")[0]  // 获取地面元素
-let pos = null
+var ground = document.getElementsByClassName("ground")[0]  // 获取地面元素
+var pos = null
 
 pos = parseInt(getComputedStyle(ground, null)["backgroundPositionX"]) // 获取元素位置
 // 移动地面
-setInterval(() => {
+function groundMove() {
     pos -= speed
     ground.style.backgroundPositionX = pos + "px"
-}, 0);
+}
 // #endregion
 
 // #region 背景云朵移动
@@ -19,102 +41,62 @@ const cloudFather = document.getElementsByClassName("cloud")[0]
 // 云朵数组
 const cloudArr = ["cloud1", "cloud2"]
 // 存储云朵
-let cloudStorage = []
+var cloudStorage = []
 // 保存云朵位置
-let cloudPosArr = []
-// 云朵生成时间
-let summomInterval
+var cloudPosArr = []
+// 场上同时出现的最大云朵数
+var maxCloudNum = 4
+// 云朵最大偏移量
+var cloudMaxOffset = 921
 
 // 创建云朵
-setInterval(() => {
-    // 云朵生成时间
-    summomInterval = Math.floor(Math.random()*8000)
-    if(summomInterval < 1000){
-        summomInterval += 8000
-    }
-    // console.log(summomInterval)
-    setTimeout(() => {
+function createCloud() {
+    // 不超过最大云朵数，添加云朵
+    if (cloudStorage && cloudStorage.length < maxCloudNum) {
         // 创建云朵元素
-        let cloud = document.createElement("div")
+        var cloud = document.createElement("div")
         // 随机数：选择云朵
-        let selCloud = Math.round(Math.random())
+        var selCloud = Math.round(Math.random())
         // 设置云朵
         cloud.setAttribute("class", cloudArr[selCloud])
-        // 随机数：选择数值位置
-        let margin = Math.floor(Math.random()*56)
+
+        // 检测本云朵与上一个云朵之间的距离
+        if (cloudStorage.length >= 1) {
+            if (cloudMaxOffset - cloudPosArr[cloudPosArr.length - 1] < 400) {
+                // 小于100px，删除该云，重新渲染
+                cloud = null
+                return
+            }
+        }
+
+        // 随机数：随机云朵高度
+        var margin = Math.floor(Math.random() * 56)
         // 设置云朵位置
         cloud.style.marginTop = margin + "px"
-        // 添加云朵到背景中
-        cloudFather.appendChild(cloud)
         // 添加云朵到列表中
         cloudStorage.push(cloud)
+        // 添加云朵到背景中
+        cloudFather.appendChild(cloud)
         // 监测云朵位置
-        let cloudPos = parseInt(getComputedStyle(cloud, null)["marginLeft"])
+        var cloudPos = parseInt(getComputedStyle(cloud, null)["marginLeft"])
         // 添加云朵位置
         cloudPosArr.push(cloudPos)
-    }, summomInterval);
-}, 1000);
+    }
+}
 
 // 云朵移动
-setInterval(() => {
-    for(let i = 0; i < cloudStorage.length; i++){
+function cloudMove() {
+    for (var i = 0; i < cloudStorage.length; i++) {
         cloudPosArr[i] -= speed
         cloudStorage[i].style.marginLeft = cloudPosArr[i] + "px"
-        if(cloudPosArr[i] < -100){
+        if (cloudPosArr[i] < -100) {
             cloudFather.removeChild(cloudStorage[i])
             cloudStorage.splice(i, 1)
             cloudPosArr.splice(i, 1)
         }
     }
-}, 0);
-// #endregion
-
-// #region 小恐龙移动动画
-let dinosaur = document.getElementsByClassName("dinosaur")[0]  // 获取小恐龙
-// 小恐龙动作偏移量
-let dinosaurPos = -1856
-// 定时器
-let timer = null
-function dinosaurMove(){
-    dinosaurPos = -1856
-    timer = setInterval(() => {
-        if(dinosaurPos === -1856){
-            // 设置背景偏移量
-            dinosaur.style.backgroundPositionX = dinosaurPos + "px"
-            dinosaurPos = -1944
-        }
-        else{
-            // 设置背景偏移量
-            dinosaur.style.backgroundPositionX = dinosaurPos + "px" 
-            dinosaurPos = -1856
-        }
-    }, 100);
 }
-dinosaurMove()
 
-/* 小恐龙跳跃动画 */
-// 添加键盘监听
-let timeout = null
-// 节流阀
-let animating = false
-document.body.addEventListener("keydown", function(event){
-    if(event.key == " "){
-        if(!animating){
-            animating = true
-            clearInterval(timer)
-            dinosaur.style.backgroundPositionX = -1680 + "px"
-            dinosaur.style.animation = "dinosaurJump 600ms 2 alternate"
-            if(parseInt(getComputedStyle(dinosaur, null)["marginBottom"]) === 0){
-                clearTimeout(timeout)
-                timeout = setTimeout(()=>{
-                    dinosaur.removeAttribute("style")
-                    animating = false
-                    dinosaurMove()
-                }, 1220)
-            }
-        }
-    }
-})
 // #endregion
 
 // #region 障碍物
@@ -123,39 +105,204 @@ const obstacle = document.querySelector(".obstacle")
 // 障碍物数组
 const obstacleArr = ["s-cactus1", "s-cactus2", "s-cactus3", "s-cactus4", "s-cactus5", "s-cactus6"]
 // 存储障碍物
-let obstacleStroage = []
-// 保存云朵位置
-let obstaclePosArr = []
+var obstacleStroage = []
+// 保存障碍物位置
+var obstaclePosArr = []
+// 场上同时出现的最大障碍物数
+const maxObstacleNum = 1
 
 // 创建障碍物
-setInterval(() => {
-    // 创建障碍物节点
-    let obstacleItem = document.createElement("div")
-    // 随机数：选择障碍物
-    let selObstacle = Math.floor(Math.random()* 6 % 6)
-    // 设置障碍物
-    obstacleItem.setAttribute("class", obstacleArr[selObstacle])
-    // 将障碍物添加到背景中
-    obstacle.appendChild(obstacleItem)
-    // 添加障碍物到列表中
-    obstacleStroage.push(obstacleItem)
-    // 监测云朵位置
-    let obstaclePos = parseInt(getComputedStyle(obstacleItem, null)["marginLeft"])
-    // 添加云朵位置
-    obstaclePosArr.push(obstaclePos)
-}, 3400);
+function createObstacle() {
+    // console.log(summomInterval)
+    if (obstacleStroage && obstacleStroage.length < maxObstacleNum) {
+        // 创建障碍物节点
+        var obstacleItem = document.createElement("div")
+        // 随机数：选择障碍物
+        var selObstacle = Math.floor(Math.random() * 6 % 6)
+        // 设置障碍物
+        obstacleItem.setAttribute("class", obstacleArr[selObstacle])
+        // 将障碍物添加到背景中
+        obstacle.appendChild(obstacleItem)
+        // 添加障碍物到列表中
+        obstacleStroage.push(obstacleItem)
+        // 监测障碍物位置
+        var obstaclePos = parseInt(getComputedStyle(obstacleItem, null)["marginLeft"])
+        // 添加障碍物位置
+        obstaclePosArr.push(obstaclePos)
+    }
+}
 
 // 障碍物移动
-setInterval(() => {
-    for(let i = 0; i < obstacleStroage.length; i++){
+function obstacleMove() {
+    for (var i = 0; i < obstacleStroage.length; i++) {
         obstaclePosArr[i] -= speed
         obstacleStroage[i].style.marginLeft = obstaclePosArr[i] + "px"
-        if(obstaclePosArr[i] < -35){
+        // console.log(obstacleStroage[i].offsetLeft);
+        if (obstaclePosArr[i] < -35) {
             obstacle.removeChild(obstacleStroage[i])
             obstacleStroage.splice(i, 1)
             obstaclePosArr.splice(i, 1)
         }
     }
-}, 0);
+}
+// #endregion
+
+// #region 小恐龙移动动画
+var dinosaur = document.getElementsByClassName("dinosaur")[0]  // 获取小恐龙
+
+/* 小恐龙跳跃动画 */
+// 小恐龙跳跃函数
+function dinosaurJump(initVelocity, initTime) {
+    dinosaurUpJump(initVelocity, initTime)
+}
+
+// 向上跳跃
+function dinosaurUpJump(initVelocity, initTime) {
+    var height = initVelocity * initTime - 0.5 * g * initTime * initTime
+    var v = initVelocity - g * initTime
+    dinosaur.style.marginBottom = height + "px"
+    initTime += 0.08
+    if (v > 0) {
+        requestAnimationFrame(() => {
+            dinosaurUpJump(initVelocity, initTime)
+        })
+    } else {
+        requestAnimationFrame(() => {
+            dinosaurDownFall(initVelocity, 0, height)
+        })
+    }
+}
+
+// 自由落体
+function dinosaurDownFall(initVelocity, initTime, maxHeight) {
+    var height = maxHeight - 0.5 * g * initTime * initTime
+    var v = g * initTime
+    dinosaur.style.marginBottom = height + "px"
+    initTime += 0.08
+    if (v <= initVelocity) {
+        requestAnimationFrame(() => {
+            dinosaurDownFall(initVelocity, initTime, maxHeight)
+        })
+    } else {
+        dinosaur.style.marginBottom = 0
+        animating = false
+        dinosaur.style.animationName = "dinosaurRun"
+    }
+}
+
+// 添加键盘监听
+// 节流阀
+var animating = false
+// 设置小恐龙最大跳跃高度 (单位：px)
+const maxJumpHeight = 130
+// 设置小恐龙跳跃时初速度
+var vertivalInitVelocity = Math.sqrt(2 * g * maxJumpHeight)
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.key == " ") {
+        if (!animating) {
+            animating = true
+            dinosaur.style.animationName = "dinosaurJump" // 小恐龙站立动画
+            dinosaurJump(vertivalInitVelocity, 0)
+        }
+    }
+})
+// #endregion
+
+// #region 移动函数
+function move() {
+    groundMove()
+    cloudMove()
+    obstacleMove()
+    requestAnimationFrame(move)
+}
+move()
+// #endregion
+
+// #region
+/* 监听碰撞 */
+function listenSpriteCollision() {
+    console.log(pixelCollision(divToCanvas(dinosaur), divToCanvas(obstacleStroage[0])));
+    if (obstacleStroage[0] && pixelCollision(divToCanvas(dinosaur), divToCanvas(obstacleStroage[0]))) {
+        console.log("碰撞");
+        gameover();
+    }
+    requestAnimationFrame(listenSpriteCollision);
+}
+listenSpriteCollision()
+
+// // /* 包围盒检测算法 */
+// function boundingBox(source, target) {
+//     // 记录原点左上、右下点坐标
+//     const sourcePos = source.getBoundingClientRect(),
+//         sLx = sourcePos.left,
+//         sLy = sourcePos.top,
+//         sRx = sourcePos.left + sourcePos.width,
+//         sRy = sourcePos.top + sourcePos.height
+//         ;
+//     // 记录目标左上、右下点坐标
+//     const targetPos = target.getBoundingClientRect(),
+//         tLx = targetPos.left,
+//         tLy = targetPos.top,
+//         tRx = targetPos.left + targetPos.width,
+//         tRy = targetPos.top + targetPos.height
+//         ;
+
+//     return !(
+//         (sLx > tRx) ||
+//         (sRx < tLx) ||
+//         (sLy > tRy) ||
+//         (sRy < tLy)
+//     )
+// }
+// // #endregion
+
+function pixelCollision(source, target) {
+    const sourceRect = source.getBoundingClientRect();
+    const targetRect = target.getBoundingClientRect();
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = Math.max(sourceRect.width, targetRect.width);
+    canvas.height = Math.max(sourceRect.height, targetRect.height);
+
+    ctx.drawImage(source, 0, 0, sourceRect.width, sourceRect.height);
+    const sourceData = ctx.getImageData(0, 0, sourceRect.width, sourceRect.height).data;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(target, 0, 0, targetRect.width, targetRect.height);
+    const targetData = ctx.getImageData(0, 0, targetRect.width, targetRect.height).data;
+
+    for (let i = 0; i < sourceData.length; i += 4) {
+        if (sourceData[i + 3] > 0 && targetData[i + 3] > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// #region div转canvas
+function divToCanvas(element) {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = element.clientWidth;
+    canvas.height = element.clientHeight;
+
+    ctx.drawImage(element, 0, 0, canvas.width, canvas.height);
+
+    return canvas;
+}
+// #endregion
+
+// #region 游戏结束
+function gameover() {
+    // 背景移动降为0
+    speed = 0
+    // 恐龙停止走动
+    dinosaur.style.animationName = "dinosaurRun"
+}
 
 // #endregion
